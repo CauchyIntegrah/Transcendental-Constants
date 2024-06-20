e9d77c56a2460b390adb87dd23a7fe859e934c75
Adam A <123107999+CauchyIntegrah@users.noreply.github.com>
2024-06-20 14:59:28 +0100

    refactor style and comment unfinished function

--- numericalapproxfore.py
+++ numericalapproxfore.py
@@ -2,9 +2,6 @@ import math
 import numpy as np
 
 def expMethod1() :  # Using the Calculus limit definition to approximate e --> comes from Compound interest
-
-
-
     getAcc = input("How accurate do you want your Euler's Number to be? Very accurate, not         accurate or Engineer's Approximation? ")
 
     veryAcc = 1000000     # Degrees of accuracy
@@ -14,8 +11,6 @@ def expMethod1() :  # Using the Calculus limit definition to approximate e --> c
     if getAcc == "Very accurate":
         print(((1+(1/veryAcc))**veryAcc))
 
-
-
     if getAcc == "Not accurate":
         print(((1+(1/notAcc))**notAcc))
 
@@ -23,19 +18,14 @@ def expMethod1() :  # Using the Calculus limit definition to approximate e --> c
         print(engineersAcc)
 
 
-
 def expMethod2() :               # Series definition of Euler's Number - derived from Maclaurin (taylor polynomial centred at 0) expansion of e^x when x = 1
-
     n = int(input("Choose a positive integer: "))
 
     eulerNum = 0
 
     for i in range(0, n):
-
         eulerNum = eulerNum + 1/math.factorial(i)
 
     print(eulerNum)
 
-
-
-def expMethod3() :
+#def expMethod3() : 
